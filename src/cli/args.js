9afd9c483355c0c8abb7796755c1053df8d86621
Pavel <verbipo@gmail.com>
2024-01-28 09:32:29 +0100

    feat: cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+    const inputedArgsArray = process.argv.slice(2);
+    const parsedArgsArray = {};
+
+    for (let i = 0; i < inputedArgsArray.length; i += 2) {
+        const propName = inputedArgsArray[i].slice(2);
+        const propValue = inputedArgsArray[i + 1];
+        parsedArgsArray[propName] = propValue;
+    }
+
+    Object.entries(parsedArgsArray).forEach(([propName, propValue]) => {
+        console.log(`${propName} is ${propValue}`);
+    });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+    const rssEnvVars = Object.fromEntries(
+        Object.entries(process.env)
+            .filter(([key]) => key.startsWith("RSS_"))
+    );
+
+    const outputLine = Object.entries(rssEnvVars)
+        .map(([key, value]) => `${key}=${value}`)
+        .join("; ");
+
+    console.log(outputLine);
 };
 
 parseEnv();
