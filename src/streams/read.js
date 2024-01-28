a4e7fce28192c1a7bf2709c7523f32751db7da5e
Pavel <verbipo@gmail.com>
2024-01-28 10:36:06 +0100

    feat: read straem

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,30 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+    const fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    try {
+        const readableStream = fs.createReadStream(fileToRead, 'utf-8');
+
+        readableStream.on('data', (chunk) => {
+            process.stdout.write(chunk);
+        });
+
+        await new Promise((resolve) => {
+            readableStream.on('end', resolve);
+        });
+
+        console.log('\nFile reading completed.');
+
+    } catch (error) {
+        throw new Error(`FS operation failed: ${error.message}`);
+    }
 };
 
 await read();
+
