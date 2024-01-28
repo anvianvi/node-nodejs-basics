51b213908d50d92c81f0bb533a246de52510749c
Pavel <verbipo@gmail.com>
2024-01-28 09:17:45 +0100

    feat: hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import fs from 'fs/promises';
+import crypto from 'crypto';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const calculateHash = async () => {
-    // Write your code here 
+
+    const filePath = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
+    try {
+        const fileContent = await fs.readFile(filePath);
+        const hash = crypto.createHash('sha256');
+        hash.update(fileContent);
+        const hashHex = hash.digest('hex');
+        console.log(`SHA256 hash for file: ${filePath}`);
+        console.log(hashHex);
+    } catch (error) {
+        throw new Error(`FS operation failed: ${error.message}`);
+    }
 };
 
 await calculateHash();
