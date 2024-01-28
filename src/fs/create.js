f5e44210c867857fc4a085eda744fc53772b3960
Pavel <verbipo@gmail.com>
2024-01-28 08:42:16 +0100

    feat: create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const pathToFile = path.join(__dirname, 'files', 'fresh.txt');
+
+    try {
+        await fs.access(pathToFile);
+        throw new Error('File already exists');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            try {
+                await fs.writeFile(pathToFile, 'I am fresh and young');
+                console.log('File created successfully');
+            } catch (writeError) {
+                throw new Error(`FS write operation failed: ${writeError.message}`);
+            }
+        } else {
+            throw new Error(`FS access operation failed: ${error.message}`);
+        }
+    }
 };
 
 await create();
